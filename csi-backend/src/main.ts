91ac985cf94285cd0d1e079673b7d15e8f70ee4d
Elio Darras <elio.darras_ext@sonepar.com>
2025-03-09 22:33:22 +0100

    add mongo uri

--- csi-backend/src/main.ts
+++ csi-backend/src/main.ts
@@ -3,10 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 import { config } from './config';
+import * as mongoose from 'mongoose';
 
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+        // Connexion à MongoDB
+        if (!config.MONGODB_URI) {
+            throw new Error('MongoDB URI is not defined. Check your environment variables.');
+        }
+    
+        try {
+            await mongoose.connect(config.MONGODB_URI);
+            console.log('✅ Connected to MongoDB');
+        } catch (error) {
+            console.error('❌ MongoDB Connection Error:', error);
+            process.exit(1); // Stop l'application si la connexion échoue
+        }
+    
     app.useGlobalPipes(new ValidationPipe({
         whitelist: true,  // Supprime les champs inconnus
         forbidNonWhitelisted: true, // Rejette toute clé non déclarée
