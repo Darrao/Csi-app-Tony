19759a2c14d72b16c0b6d81973bb648f61955618
Elio Darras <elio.darras_ext@sonepar.com>
2025-03-13 17:33:10 +0100

    modif formulaire et formulaire token, essai de modif du back pour gerer la modif doctorant as an admin mais flop

--- csi-backend/src/config.ts
+++ csi-backend/src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:3001',
     BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:3000',
-    // MONGODB_URI: process.env.MONGODB_URI || 'fallback-uri',
+    MONGODB_URI: process.env.MONGODB_URI || 'fallback-uri',
     // local
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/csi'
+    // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/csi'
   };
--- csi-frontend/src/services/api.ts
+++ csi-frontend/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-//   baseURL: "https://csi.edbiospc.fr/api", // 🔥 On force l'URL correcte du backend
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://csi.edbiospc.fr/api", // 🔥 On force l'URL correcte du backend
+//   baseURL: "http://localhost:3000/api",
 //   baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
