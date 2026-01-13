a1b240aff99dd5ea09c0b84c320aa0ca5f19ec8d
Darrao <elio.darras@gmail.com>
2026-01-13 18:06:57 +0100

    feat: Add AWS EC2 deployment workflow

--- csi-backend/src/main.ts
+++ csi-backend/src/main.ts
@@ -71,7 +71,8 @@ async function bootstrap() {
     console.error('❌ Fichier non trouvé !');
   }
 
-  await app.listen(3000, '0.0.0.0');
-  console.log(`🚀 Serveur démarré sur http://localhost:3000`);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Serveur démarré sur http://localhost:${port}`);
 }
 bootstrap();
